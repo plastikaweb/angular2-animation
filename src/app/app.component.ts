34e8c87ea6f88189bf84e181af70a566005c6572
Plastikaweb <info@plastikaweb.com>
2017-01-20 08:25:18 +0100

    advanced transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,39 @@ import {Component, trigger, state, style, transition, animate} from '@angular/co
                 'background-color': 'blue',
                 transform: 'translateX(100px)'
             })),
-            transition('normal => highlighted', animate(300)),
-            transition('highlighted => normal', animate(800))
+            transition('normal <=> highlighted', animate(300))
+        ]),
+        trigger('wildState', [
+            state('normal', style({
+                'background-color': 'red',
+                transform: 'translateX(0) scale(1)'
+            })),
+            state('medium', style({
+                'background-color': 'blue',
+                transform: 'translateX(300px) scale(1.5)'
+            })),
+            state('highlighted', style({
+                'background-color': 'yellow',
+                transform: 'translateX(-50px) scale(0.5)'
+            })),
+            transition('normal => medium', animate(300)),
+            transition('medium => normal', animate(800)),
+            transition('highlighted <=> *', animate(500))
         ])
     ]
 } )
 export class AppComponent {
     state = 'normal';
+    wildState = 'normal';
     list = ['Milk', 'Sugar', 'Bread'];
 
     onAnimate() {
         this.state  = this.state === 'normal' ? 'highlighted' : 'normal';
+        this.wildState  = this.state === 'normal' ? 'highlighted' : 'normal';
+    }
+
+    onShrink() {
+        this.wildState  = this.wildState === 'normal' ? 'medium' : 'normal';
     }
 
     onAdd(item) {
