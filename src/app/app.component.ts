0b4af6f07b951ab5d09938fe1f437085f3c4deb9
Plastikaweb <info@plastikaweb.com>
2017-01-20 08:10:22 +0100

    first simple animation

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import {Component, trigger, state, style, transition, animate} from '@angular/core';
+
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    animations: [
+        trigger('divState', [
+            state('normal', style({
+                'background-color': 'red',
+                transform: 'translateX(0)'
+            })),
+            state('highlighted', style({
+                'background-color': 'blue',
+                transform: 'translateX(100px)'
+            })),
+            transition('normal => highlighted', animate(300)),
+            transition('highlighted => normal', animate(800))
+        ])
+    ]
+} )
+export class AppComponent {
+    state = 'normal';
+    list = ['Milk', 'Sugar', 'Bread'];
+
+    onAnimate() {
+        this.state  = this.state === 'normal' ? 'highlighted' : 'normal';
+    }
+
+    onAdd(item) {
+        this.list.push( item );
+    }
+
+    onDelete(item) {
+        this.list.splice( this.list.indexOf( item ), 1 );
+    }
+}
