6590cc2ba23cf1e6a6a77ec70d0a233ffd4c72bc
Plastikaweb <info@plastikaweb.com>
2017-01-22 10:42:09 +0100

    keyframes animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, trigger, state, style, transition, animate} from '@angular/core';
+import {Component, trigger, state, style, transition, animate, keyframes} from '@angular/core';
 
 @Component( {
     selector: 'app-root',
@@ -38,6 +38,61 @@ import {Component, trigger, state, style, transition, animate} from '@angular/co
                     borderRadius: '50px'
                 })),
                 animate(500)
+            ])
+        ]),
+        trigger('list1', [
+            state('in', style({
+                opacity: 1,
+                transform: 'translateX(0)'
+            })),
+            transition('void <=> *', [
+                style({
+                    opacity: 0,
+                    transform: 'translateX(-100px)'
+                }),
+                animate(100)
+            ]),
+            transition('* <=> void', [
+                animate(300, style({
+                    transform: 'translateX(100px)',
+                    opacity: 0
+                }))
+            ])
+        ]),
+        trigger('list2', [
+            state('in', style({
+                opacity: 1,
+                transform: 'translateX(0)'
+            })),
+            transition('void <=> *', [
+                animate(1000, keyframes([
+                    style({
+                        transform: 'translateX(-100px)',
+                        opacity: 0,
+                        offset: 0
+                    }),
+                    style({
+                        transform: 'translateX(-50px)',
+                        opacity: 0.5,
+                        offset: 0.3
+                    }),
+                    style({
+                        transform: 'translateX(-20px)',
+                        opacity: 1,
+                        offset: 0.8
+                    }),
+                    style({
+                        transform: 'translateX(0px)',
+                        opacity: 1,
+                        offset: 1
+                    })
+                ]))
+            ]),
+            transition('* <=> void', [
+                animate(300, style({
+                    transform: 'translateX(100px)',
+                    opacity: 0
+                }))
             ])
         ])
     ]
